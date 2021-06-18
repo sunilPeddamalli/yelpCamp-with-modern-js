aaf20770eb72a7ee677aa007c5052bbbbfedbc7b
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-06-17 17:20:12 -0700

    installed and connected mongoose,created model folder with campground js file and created campground schema and model which was then exported to app.js file to insert data into DB using the schema and get route

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost/yelpcamp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('connected to MongoDB');
+    }).catch((err) => {
+        console.log('MongoDB Error!');
+        console.log(err);
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -9,6 +19,12 @@ app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+app.get('/makeCamp', (req, res) => {
+    const campground = new Campground({ title: 'firstCampground', price: 9, description: 'This is the first Campground!' });
+    campground.save();
+    res.send(campground);
+});
 
 app.listen(3000, () => {
     console.log('listening to Port 3000')
