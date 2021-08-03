655a711244afe29685d644d961dc7e09c12310ba
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-08-02 19:39:06 -0700

    created login get route to display form to be submitted to login post route which uses passport.authenticare middleware function to compare date with DB

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchError = require('../utils/catchError');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -12,14 +13,21 @@ router.post('/register', catchError(async (req, res) => {
         const { username, email, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
         req.flash('success', 'Welcome to YelpCamp!');
         res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
     }
-
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back');
+    res.redirect('/campgrounds')
+});
 
 module.exports = router;
