2fad3a147ab2817e0dedd4f31eb48c636d83e42d
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-08-03 18:54:43 -0700

    added req.login method to register routes to log in user and create session containing user details

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchError(async (req, res) => {
+router.post('/register', catchError(async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp! Kindly login');
-        res.redirect('/login');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
