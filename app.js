4d3446061b7ccf0037a2502f377633a929141481
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-08-03 19:29:05 -0700

    kept track of the URL in middleware funcition in middleware file and then used that URL to redirect in login route so that user returns back thier original URL

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    console.log(req.session);
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
--- middleware.js
+++ middleware.js
@@ -1,8 +1,10 @@
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You need to login');
         return res.redirect('/login');
     }
     next();
 };
+
