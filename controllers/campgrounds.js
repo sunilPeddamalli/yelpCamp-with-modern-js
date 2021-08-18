0fe65e30e43df7357af96e158a4897b24e5336e7
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-08-17 17:17:21 -0700

    to delete image from edit form - display images in edit form, created checkbox for each image and add value as images filename and name as deleteImages[array] so all the values gets added to this array and is added to req.body. To remove the selected image from db- use campgroud.updateOne with query to pull from campground image array where the filename matches the deletearray filename. To remove from clodinary, require exporter cloudinary variable from controller and loop over the deletedarray and then use cloudinary.uploader.destroy(el)

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary')
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -56,12 +57,20 @@ module.exports.updateCampground = async (req, res) => {
     //     req.flash('error', "You don't have premission")
     //     return res.redirect(`/campgrounds/${campground._id}`)
     // }
+    console.log(req.body)
     const campground = await Campground.findByIdAndUpdate(id, req.body.campground, { useFindAndModify: false });
     const imgs = req.files.map(f => {
         return { url: f.path, filename: f.filename }
     });
     campground.images.push(...imgs);
     await campground.save();
+    if (req.body.deleteImages) {
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+    };
+
     req.flash('success', 'Successfully updated campground')
     res.redirect(`/campgrounds/${campground._id}`);
 };
