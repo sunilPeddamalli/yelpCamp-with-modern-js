b7a68018b194436b16ff5ac4f88a513172877aae
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-08-24 20:52:29 -0700

    Added mapbox default marker and displayed it on the map based on the location entered while creating a campground

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+new mapboxgl.Marker({ color: 'red' })
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
