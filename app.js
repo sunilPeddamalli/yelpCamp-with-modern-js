9ed76e65ae67853d0e7ab0e121377257cb2c758a
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2021-09-20 12:43:09 -0700

    added start script and heroku server port to listen

--- app.js
+++ app.js
@@ -168,6 +168,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('listening to Port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`listening to Port ${port}`)
 });
